841f2391d522d111a49becba0fce945f741786f3
Ramon Diaz <r.diaz@intellisys.com.do>
2024-10-10 14:39:37 -0400

    adding validate email

--- backend/models/Person.js
+++ backend/models/Person.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
 const PersonSchema = new Schema(
   {
     name: { type: String, required: true },
@@ -14,7 +19,15 @@ const PersonSchema = new Schema(
       },
       required: [true, "Phone number is required"],
     },
-    email: { type: String, unique: true, required: true },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
     password: { type: String, required: true },
   },
   { timestamps: true, required: true }
