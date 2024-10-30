b461e3db19a27fae88bd0afad52a55367134d123
Ramon Diaz <r.diaz@intellisys.com.do>
2024-10-30 13:26:29 -0400

    creating client table

--- frontend/src/app/client/page.js
+++ frontend/src/app/client/page.js
@@ -6,9 +6,10 @@ import { useRouter } from "next/navigation";
 import ClientDetails from "../components/ClientDetails";
 import ClientForm from "../components/ClientForm";
 import Appbar from "../components/Appbar";
+import ClientTable from "../components/ClientTable";
 
 export default function Client() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const router = useRouter();
   const goToDashboard = () => {
     router.push("/dashboard");
@@ -32,6 +33,7 @@ export default function Client() {
         return;
       }
 
+      try {
       const res = await fetch("http://localhost:3546/api/client", {
         method: "GET",
         headers: {
@@ -42,6 +44,9 @@ export default function Client() {
 
       const result = await res.json();
       setData(result);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
     };
 
     fetchData();
@@ -52,10 +57,11 @@ export default function Client() {
       <Appbar />
       <div className="" style={{ textAlign: "center", padding: "50px" }}>
         <ClientForm />
-        {data &&
+        {/* {data &&
           data.map((client) => (
             <ClientDetails key={client._id} client={client} />
-          ))}
+          ))} */}
+          {data.length > 0 ? <ClientTable client={data} /> : <p>No clients found.</p>}
       </div>
     </div>
   );
