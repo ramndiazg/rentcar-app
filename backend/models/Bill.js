d33d260c2b8885d171f9a0fd8eb4673e5587e010
Ramon Diaz <r.diaz@intellisys.com.do>
2024-10-31 18:24:14 -0400

    creating rent schema

---
+++ backend/models/Bill.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const RentSchema = new Schema(
+  {
+    client: { type: mongoose.Schema.Types.ObjectId, ref: "Client", required: true },
+    vehicle: { type: mongoose.Schema.Types.ObjectId, ref: "Vehicle", required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    rentDays: { type: Number, required: true },
+    rentAmount: { type: Number, required: true },
+    rentStatus: {
+      type: String,
+      enum: ["paid", "pending", "cancelled"],
+      default: "pending",
+      message: "{VALUE} is not supported",
+      required: true
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Rent", RentSchema);
